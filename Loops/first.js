14ec827f4cf8a99b262104933531abe23f3b8a9b
Sandip <154871379+student-sandip@users.noreply.github.com>
2025-03-12 07:19:35 +0000

    first.js

---
+++ Loops/first.js
@@ -0,0 +1,36 @@
+// for loop ---
+
+// prints patterns of stars --- 
+
+for (let i = 1; i >= 5; i++) {
+    let pattern = '';
+    for (let j = 1; j <= i; j++) {
+        pattern += '*';
+    }
+    console.log(pattern);
+}
+
+
+const myArr = ["sn", "Senu", "Sneha", "Sandip"]
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+    console.log(element);
+}
+
+// while loop ---
+let i = 1;
+while(i <= 10){
+    console.log(i);
+    i++;
+}
+
+let OneArray = ["senu", "sneha", "sandip"];
+let arr = 0;
+
+while(s < OneArray.length){
+    console.log(OneArray[arr]);
+    arr++;
+}
+
+// baki sob loops prai prai same but 
+// we have to learn about for-each loop in next file.
